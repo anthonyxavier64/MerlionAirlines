a571e32f505190a21fa7f64add742fda902accb4
Yap Pei Zhen <yappeizhen@u.nus.edu>
2020-10-15 18:03:17 +0800

    second commit with files

---
+++ MerlionAirlines-ejb/src/java/ejb/session/stateful/InventorySessionBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateful;
+
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author yappeizhen
+ */
+@Stateful
+public class InventorySessionBean implements InventorySessionBeanRemote, InventorySessionBeanLocal {
+
+    public void print() {
+        System.out.println("HELLO");
+    }
+}
