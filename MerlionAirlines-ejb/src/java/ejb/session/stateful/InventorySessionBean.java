7e23612fbffd0c316d868e47c9e3d2747f5b28a9
anthony <anthony_xavier64@hotmail.com>
2020-10-15 20:34:14 +0800

    changed gitignore

--- MerlionAirlines-ejb/src/java/ejb/session/stateful/InventorySessionBean.java
+++ MerlionAirlines-ejb/src/java/ejb/session/stateful/InventorySessionBean.java
@@ -13,10 +13,5 @@ import javax.ejb.Stateful;
  */
 @Stateful
 public class InventorySessionBean implements InventorySessionBeanRemote, InventorySessionBeanLocal {
-<<<<<<< HEAD
-    private int numSeats;
-
-=======
-    abcde
->>>>>>> af5ddc1a3d6da4e41f41e758cf06ca1caa260416
+    
 }
