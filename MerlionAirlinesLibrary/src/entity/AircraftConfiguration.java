d7dc71c64ba81a4fee0489e6d0109cab087475fa
anthony <anthony_xavier64@hotmail.com>
2020-11-06 21:05:46 +0800

    Merge origin/master
    Conflicts:
    	MerlionAirlinesLibrary/src/entity/AircraftConfiguration.java
    	MerlionAirlinesLibrary/src/entity/AircraftType.java
    	MerlionAirlinesLibrary/src/entity/Airport.java
    	MerlionAirlinesLibrary/src/entity/CabinClassConfiguration.java
    	MerlionAirlinesLibrary/src/entity/Customer.java
    	MerlionAirlinesLibrary/src/entity/Fare.java
    	MerlionAirlinesLibrary/src/entity/Flight.java
    	MerlionAirlinesLibrary/src/entity/FlightReservation.java
    	MerlionAirlinesLibrary/src/entity/FlightRoute.java
    	MerlionAirlinesLibrary/src/entity/FlightSchedule.java
    	MerlionAirlinesLibrary/src/entity/FlightSchedulePlan.java
    	MerlionAirlinesLibrary/src/entity/Seat.java
    	MerlionAirlinesLibrary/src/entity/SeatInventory.java

--- MerlionAirlinesLibrary/src/entity/AircraftConfiguration.java
+++ MerlionAirlinesLibrary/src/entity/AircraftConfiguration.java
@@ -28,7 +28,6 @@ public class AircraftConfiguration implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(unique = true, nullable = false)
     private Long aircraftConfigurationID;
     
     @Column(unique = true, nullable = false)
