0563d1442bf2545da4d59663c0c42210303b74ee
Anthony Xavier <e0406854@u.nus.edu>
2020-11-15 15:56:08 +0000

    commit

--- FRSReservationClient/src/java/frsreservationclient/Main.java
+++ FRSReservationClient/src/java/frsreservationclient/Main.java
@@ -7,6 +7,7 @@ package frsreservationclient;
 
 import ejb.session.stateless.AirportSessionBeanRemote;
 import ejb.session.stateless.CustomerSessionBeanRemote;
+import ejb.session.stateless.FareSessionBeanRemote;
 import ejb.session.stateless.FlightSchedulePlanSessionBeanRemote;
 import ejb.session.stateless.FlightScheduleSessionBeanRemote;
 import ejb.session.stateless.PassengerSessionBeanRemote;
@@ -20,6 +21,8 @@ import javax.ejb.EJB;
 public class Main {
 
     @EJB
+    private static FareSessionBeanRemote fareSessionBean;
+    @EJB
     private static PassengerSessionBeanRemote passengerSessionBean;
     @EJB
     private static SeatSessionBeanRemote seatSessionBean;
@@ -31,12 +34,13 @@ public class Main {
     private static AirportSessionBeanRemote airportSessionBeanRemote;
     @EJB
     private static CustomerSessionBeanRemote customerSessionBeanRemote;
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        MainApp mainApp = new MainApp(customerSessionBeanRemote, airportSessionBeanRemote, flightScheduleSessionBeanRemote, flightSchedulePlanSessionBean, passengerSessionBean, seatSessionBean);
+        MainApp mainApp = new MainApp(customerSessionBeanRemote, airportSessionBeanRemote, flightScheduleSessionBeanRemote, flightSchedulePlanSessionBean, passengerSessionBean, seatSessionBean, fareSessionBean);
         mainApp.run();
     }
 
